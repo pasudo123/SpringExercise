d3a79a4cdc9943dbb50b073e94dac730c229e436
pasudo123 <pasudo222@gmail.com>
2018-01-19 11:12:20 +0900

    Update About httpServletRequest & Annotation @RequestParam

--- springex2/src/main/java/com/javalec/ex/HomeController.java
+++ springex2/src/main/java/com/javalec/ex/HomeController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Handles requests for the application home page.
@@ -46,5 +47,14 @@ public class HomeController {
 		model.addAttribute("pw", pw);
 		
 		return "board/confirmId";
+	}
+	
+	@RequestMapping(value="board/checkId")
+	public String checkId(@RequestParam("id") String id, @RequestParam("pw") int pw, Model model){
+		
+		model.addAttribute("identify", id);
+		model.addAttribute("password", pw);
+		
+		return "board/checkId";
 	}
 }
