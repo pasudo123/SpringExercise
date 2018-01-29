87b5adea7197d5f9fda254408a84af9ea6df06a4
pasudo123 <pasudo123@naver.com>
2018-01-29 20:23:18 +0900

    Update Exercise

--- PencilEx/src/main/java/com/javalec/ex/Pencil6B.java
+++
@@ -1,8 +0,0 @@
-package com.javalec.ex;
-
-public class Pencil6B implements Pencil {
-	@Override
-	public void use(){
-		System.out.println("6B 연필 단독으로 쓰이고 있습니다.");
-	}
-}
--- PencilEx/src/main/java/com/javalec/ex/Pencil6BWithEraser.java
+++
@@ -1,8 +0,0 @@
-package com.javalec.ex;
-
-public class Pencil6BWithEraser implements Pencil {
-	@Override
-	public void use(){
-		System.out.println("6B와 함께 지우개도 같이 쓰이고 있습니다.");
-	}
-}
